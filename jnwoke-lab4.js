807fe2f1e7218cbe7d69230b32df5fd5012e72aa
joenwoke <jnwoke@hawk.iit.edu>
2025-04-02 21:35:26 -0500

    Updated Js file to change hero subline

--- jnwoke-lab4.js
+++ jnwoke-lab4.js
@@ -4,4 +4,7 @@
 (function () {
 	//Requirement 1 -- Change the hero message
 	document.querySelector('h1').textContent = "Uplift Your Brand with Stellar Marketing";
-});
+	
+	//Reuqirement 2 -- Chnage the line below hero headline
+	document.querySelector('p').innerHTML = 'Utilize cutting-edge strategies from Stellar Marketing to help your business <strong><em>thrive and excel.</em></strong>';
+})();
