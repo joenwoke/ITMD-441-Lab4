943b9d505ed922986793bfa793ca45be130dfbe3
joenwoke <jnwoke@hawk.iit.edu>
2025-04-02 21:30:19 -0500

    Update JS file to change the hero message

--- jnwoke-lab4.js
+++ jnwoke-lab4.js
@@ -1,1 +1,7 @@
+// Joseph Nwoke
+//ITMD 441-01 Undergraduate Student
 
+(function () {
+	//Requirement 1 -- Change the hero message
+	document.querySelector('h1').textContent = "Uplift Your Brand with Stellar Marketing";
+});
